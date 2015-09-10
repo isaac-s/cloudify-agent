704164d1dcf53f16b20da3faf71051cbf4f851bf
fogelomer <omer@gigaspaces.com>
2015-09-10 11:48:57 +0300

    only windows changes. no command check, no linux changes

--- cloudify_agent/tests/api/pm/__init__.py
+++ cloudify_agent/tests/api/pm/__init__.py
@@ -383,28 +383,28 @@ class BaseDaemonProcessManagementTest(BaseDaemonLiveTestCase):
                 os.environ['PATH'] = '{0}{1}{2}'.format(
                     VIRTUALENV, os.pathsep, os.environ['PATH'])
 
-            daemon = self.create_daemon()
-            daemon.create()
-            daemon.configure()
-            self.installer.install(
-                os.path.join(resources.get_resource('plugins'),
-                             'mock-plugin'))
-            daemon.register('mock-plugin')
-            daemon.start()
+        daemon = self.create_daemon()
+        daemon.create()
+        daemon.configure()
+        self.installer.install(
+            os.path.join(resources.get_resource('plugins'),
+                         'mock-plugin'))
+        daemon.register('mock-plugin')
+        daemon.start()
 
-            def _get_env_var(var):
-                return self.celery.send_task(
-                    name='mock_plugin.tasks.get_env_variable',
-                    queue=daemon.queue,
-                    args=[var]).get(timeout=5)
+        def _get_env_var(var):
+            return self.celery.send_task(
+                name='mock_plugin.tasks.get_env_variable',
+                queue=daemon.queue,
+                args=[var]).get(timeout=5)
 
-            def _check_env_path():
-                _path = _get_env_var('PATH')
-                print(_path)
-                cnt = _path.count(VIRTUALENV)
-                print(cnt)
-                self.assertEqual(cnt, 1)
-            _check_env_path()
+        def _check_env_path():
+            _path = _get_env_var('PATH')
+            print(_path)
+            cnt = _path.count(VIRTUALENV)
+            print(cnt)
+            self.assertEqual(cnt, 1)
+        _check_env_path()
 
     def test_conf_env_variables(self):
         if VIRTUALENV in os.environ['PATH']:
